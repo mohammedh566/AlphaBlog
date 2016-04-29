05c32bfdde532afdae0c2799f87111843099a696
mohammed hamza <mohammedh566@gmail.com>
2016-04-29 20:06:54 +0000

    Fix create articles,add flash and error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: {minimum:3, maximum:10}
+    validates :title, presence: true, length: {minimum:3, maximum:26}
      validates :description, presence: true, length: {minimum:10, maximum:100}
     
 end
