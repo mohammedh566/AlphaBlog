bfdac53bb9c5316fd6aace436d237cd930e9860f
mohammed hamza <mohammedh566@gmail.com>
2016-05-03 15:59:30 +0000

    Added user table and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User <ActiveRecord::Base
-    
+    validates :username, presence:true, length: {minimum:5, mazimum: 25}, uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence:true, length: {minimum:15, mazimum: 50}, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX }
 end
