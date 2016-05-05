c4182864725aa5e166909b76c25af2ee0ecb6b44
mohammed hamza <mohammedh566@gmail.com>
2016-05-05 11:01:37 +0000

    Add association bw user and articles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User <ActiveRecord::Base
+    has_many :articles
+    before_save {self.email = email.downcase }
     validates :username, presence:true, length: {minimum:5, mazimum: 25}, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence:true, length: {minimum:15, mazimum: 50}, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX }
