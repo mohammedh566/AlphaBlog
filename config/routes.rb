a1f1bd827c673e6dbabfadd4f63d04c9380d235c
mohammed hamza <mohammedh566@gmail.com>
2016-05-09 06:46:52 +0000

    Completed authetication and restricting users

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     resources :users, except:[:new]
     
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout' , to: 'sessions#destroy'
     #same as above
     #post 'users', to: 'users#create'
   
